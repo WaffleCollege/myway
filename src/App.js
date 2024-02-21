2dc3582dc7de67ea8ada4688df1e8cc8e4acddf8
kitap <yu.goto321@gmail.com>
2024-02-21 10:49:01 +0900

    homeをentire.jsに設定。他のも変更

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 
 import Post from './components/post/Post';
 import SignUpLogIn from "./components/SignUp_logIn/SignUpLogIn";
+import Entire from "./components/entire-display/entire";
+
 
 function App() {
   return (
     <BrowserRouter>
      <div className="app">
-        <Link to="/">Post</Link>
+        <Link to="/">entire</Link>
           <br />
         <Link to="/SignUp_logIn">会員登録・ログイン</Link>
-        
+        <br />
+       <Link to ="/post">新規投稿</Link>
         
         <Routes>
-          <Route path = "/" element = {<Post />}/>
+          <Route path = "/" element = {<Entire />}/>
           <Route path = "/SignUp_LogIn" element={<SignUpLogIn />}/>
+          <Route path = "/post" element = {<Post />}/>
+
         </Routes>
       </div>
     </BrowserRouter>
