2eab64bc154c0875cc2dbc79af9d8276ce674875
kitap <yu.goto321@gmail.com>
2024-03-03 16:33:14 +0900

    検索box

--- src/components/entire-display/input.js
+++ src/components/entire-display/input.js
@@ -1,20 +1,28 @@
 // React から useState フックをインポート
 import { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import "./entire.css"
 
 export const Txt = () => {
   // 初期値: 空文字列 ''
-  const [text, setText] = useState('検索');
+  const [text, setText] = useState('');
 
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
+        <div className='search'>
+        <div className='search-img'>
+          <SearchIcon/>
+        </div>
         <input
-          type="text"
+          type="search"
+          placeholder='   キーワードで検索'
           // text ステートが持っている入力中テキストの値を value として表示
           value={text}
           // onChange イベント（＝入力テキストの変化）を text ステートに反映する
           onChange={(e) => setText(e.target.value)}
         />
+        </div>
         {/* <input type="submit" />   */}
       </form>
 
