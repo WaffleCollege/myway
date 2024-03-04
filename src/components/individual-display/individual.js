8fb82379b8ea90a27deb443cb5dfd8ed4dc91938
sakurako <sakurakoyachita@gmail.com>
2024-03-04 23:36:27 +0900

    Modal作成(firebaseの連携は一旦コメントアウトしています)

--- src/components/individual-display/individual.js
+++ src/components/individual-display/individual.js
@@ -18,6 +18,9 @@ import { doc, collection, getDocs } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const Individual = () => {
+  /*postIdを格納するstate*/
+  const [postId, setPostId] = useState('');
+
   /*setPosts関数宣言*/
   const [posts, setPosts] = useState([]);
 
@@ -25,21 +28,22 @@ const Individual = () => {
   useEffect(() => {
     const postData = collection(db, "posts");
     getDocs(postData).then((querySnapshot) => {
-    setPosts(querySnapshot.docs.map((doc) => ({...doc.data()})));
+    setPosts(querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id })));
   });
 
   /*リアルタイムで取得*/
   onSnapshot(postData, (post) => {
-    setPosts(post.docs.map((doc) => ({...doc.data()})));
+    setPosts(post.docs.map((doc) => ({...doc.data(), id: doc.id })));
     });
   }, []);
-  
+
   return (
     <div className="container">      
-      <main>
-        {/*postデータ表示*/}
+       <main>
+        個別閲覧画面
+        {/*postデータ表示
         {posts.map((post) => (
-        <div>          
+        <div key={post.id}>          
         <div className="indiv_intro_box">
           <div className="indiv_title">
             <h1>{post.title}</h1>
@@ -54,34 +58,40 @@ const Individual = () => {
      
         <div className="indiv_course_box">
           <ul className='timeline'>
-            <li>
-              <div className='timeline_content'>
-                <div className='flex'>
-                  <figure className='mappin_img'>
-                    <FaMapMarkerAlt />
-                    </figure>
-                  <div className="indiv_spotName">
-                    <h3>{post.spotName}</h3>
-                    </div>
+            {post.spotName.map((spot, index) => (
+               <li key={index}>
+               <div className='timeline_content'>
+                 <div className='flex'>
+                   <figure className='mappin_img'>
+                     <FaMapMarkerAlt />
+                     </figure>
+                   <div className="indiv_spotName">
+                     <h3>{spot}</h3>
+                     </div>
+                 </div>
+           
+                 <div className="indiv_spotIntroduce">
+                   <h3>{post.spotIntroduce[index]}</h3>
+                   </div>
+                 <div className="indiv_image">
+                   <img src={post.img}/>
+                   </div>
                 </div>
-          
-                <div className="indiv_spotIntroduce">
-                  <h3>{post.spotIntroduce}</h3>
-                  </div>
-                <div className="indiv_image">
-                  {/*imgタグstorageとの連携要確認 */}
-                  <img src={post.img}/>
-                  </div>
-               </div>
-              
-            </li>
+               
+             </li> 
+            ))}
+            
           </ul>
           
         </div>
         </div>
           ))}
-      </main>      
-    </div>
+         */}
+      </main>
+      
+        
+        
+    </div> 
   )
 }
 
