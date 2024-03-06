c9b5240bb7b3b5ea95e06caf0da0e9dcfb4b538d
sakurako <sakurakoyachita@gmail.com>
2024-03-07 04:46:52 +0900

    conflict解消

--- src/components/entire-display/postdisplay.js
+++ src/components/entire-display/postdisplay.js
@@ -2,29 +2,27 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import "./entire.css";
 
-function BoxSystemProps({ title, image }) {
-    return (        
+function BoxSystemProps({title,image})  {
+  
+    return (
         <Box className="boxes"
-            height={200}
-            width={200}
-            my={4}
+            //  height={400}
+            //  width={300}
+            //my={4}
             display="flex"
-            flexDirection="column"
             alignItems="center"
             gap={4}
             p={2}
-            sx={{ border: '2px solid  #9DDCDC', position: 'relative' }}
+            sx={{ border: '2px solid grey' }}
         >
-    
-            <div className='boxtitle' style={{ position: 'absolute', zIndex: 1, top: '50%', transform: 'translateY(-50%)' }}>
-                <h1 style={{ margin: 0 }}>{title}</h1>
-            </div>
-            <div className='boximage' style={{ width: '100%', height: '100%', overflow: 'hidden', textAlign: 'center', position: 'relative', zIndex: 0 }}>
-                <img src={image} alt="Uploaded Image" style={{ width: '100%', height: 'auto', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',opacity: 1  }} />                
-            </div>
+            <p>{title}</p>
+            <img src={image} 
+            height={300}
+            width={200}
             
+            alt="Uploaded Image" />
         </Box>
-    )
+    );
 }
 
 export default BoxSystemProps;
